30bca4d98ed6f2e192f17224f5b24a2461691994
vitantonio99 <vitantoniopasqualicchio0@gmail.com>
2026-05-15 14:57:22 +0200

    02-arrays-bonus - second exercise performed

--- 02-arrays-bonus/script-bonus.js
+++ 02-arrays-bonus/script-bonus.js
@@ -24,13 +24,32 @@ console.log('Elenco insegnanti invertito: ', reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = [];
+
+for (let i=0; i < teachers.length; i++){
+  if (teachers[i].length >= 5) longNames[longNames.length] = teachers[i];
+}
+
+console.log('Elenco con solo insegnanti con un nome di lunghezza >= a 5 caratteri: ', longNames);
+
+//-----------------------------------------------------------------------------------------------------//
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+
+
+//-----------------------------------------------------------------------------------------------------//
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+
 const isFabioPresent = null;
 
+//-----------------------------------------------------------------------------------------------------//
+
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+
 const teachersString = null;
+
+//-----------------------------------------------------------------------------------------------------//
